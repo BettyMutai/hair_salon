7118e0f0ccb043226f8798d8370b7ce27255cd28
Betty Mutai <bettymutai00@gmail.com>
2017-07-14 11:41:20 +0300

    Added routes and flow of my app

--- app.rb
+++ app.rb
@@ -5,5 +5,32 @@ require("sinatra")
   require("./lib/stylist")
   require("pg")
 
-
   DB = PG.connect({:dbname => "hair_salon"})
+
+  get("/") do
+    @stylists = Stylist.all()
+    erb(:index)
+  end
+
+  post("/stylists") do
+    stylist_name = params.fetch("stylist_name")
+    stylist = Stylist.new({:stylist_name => stylist_name, :id => nil})
+    stylist.save()
+    @stylists = Stylist.all()
+    erb(:index)
+  end
+
+  get("/stylists/:id") do
+    @stylist = Stylist.find(params.fetch("id").to_i())
+    erb(:stylist)
+  end
+
+  post("/clients") do
+    client_name = params.fetch("client_name")
+    contact = params.fetch("contact")
+    stylist_id = params.fetch("stylist_id").to_i()
+    client = Client.new({:client_name => client_name, :contact => contact, :stylist_id => stylist_id})
+    client.save()
+    @stylist = Stylist.find(stylist_id)
+    erb(:stylist)
+  end
