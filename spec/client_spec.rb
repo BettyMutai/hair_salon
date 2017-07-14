8d701a8abeb5be13af06b5192aa74fdcc67c55a6
Betty Mutai <bettymutai00@gmail.com>
2017-07-14 10:53:39 +0300

    Changed attribute name to client_name

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,33 +8,33 @@ describe(Client) do
   end
   describe("#save") do
     it("adds a client to the array of saved clients") do
-      test_client = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      test_client = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
       test_client.save()
       expect(Client.all()).to(eq([test_client]))
     end
   end
   describe("#name") do
     it("lets you see the name of a client") do
-      test_client = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
-      expect(test_client.name()).to(eq("Paul"))
+      test_client = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      expect(test_client.client_name()).to(eq("Paul"))
     end
   end
   describe("#contact") do
     it("lets you see the contact of a client") do
-      test_client = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      test_client = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
       expect(test_client.contact()).to(eq("0722456283"))
     end
   end
   describe("#stylist_id") do
     it("lets you see the stylist id") do
-      test_client = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      test_client = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
       expect(test_client.stylist_id()).to(eq(1))
     end
   end
   describe("#==") do
     it("is the same client if they have the same name and contact") do
-      client1 = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
-      client2 = Client.new({:name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      client1 = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
+      client2 = Client.new({:client_name => "Paul", :contact => "0722456283", :stylist_id => 1})
       expect(client1).to(eq(client2))
     end
   end
